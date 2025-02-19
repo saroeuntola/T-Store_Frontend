595fd32947be66330583e3e46ff02df2bd9ac545
SaroeunTola <tolasweatboy@gmail.com>
2025-02-19 20:50:10 +0700

    update

---
+++ src/config_API/category_api.jsx
@@ -0,0 +1,75 @@
+import { baseURL } from "service/baseURL";
+
+const getCategory = async (token) => {
+ 
+    try {
+      const response = await baseURL.get("/category", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  };
+
+
+const getCategoryById = async (id) => {
+  try {
+    const response = await baseURL.get(`/category/${id}`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+const createCategory = async (token, body) => {
+  try {
+    const response = await baseURL.post("/category", body, {
+      header: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+const UpdateCategoryUser = async (token, id, body) => {
+  try {
+    const response = await baseURL.post(`/category/${id}`, body, {
+      headers: {
+    
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+const deleteCategory = async (token, id) => {
+  try {
+    const response = await baseURL.delete(`/cagetory/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+
+
+};
+
+export {
+  getCategory,
+  getCategoryById,
+  createCategory,
+  UpdateCategoryUser,
+  deleteCategory,
+};
